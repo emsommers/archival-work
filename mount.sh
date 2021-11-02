9ecfc874b2effca4d05e2a91406f93c4b56b49fc
Emily Sommers <sommers.emily@gmail.com>
2021-11-02 10:32:18 -0400

    Commit on main

---
+++ mount.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo mount.cifs -v -o username=sommerse,uid=sommerse //190-77.library.utoronto.ca/ArchivesDigital$ /home/sommerse/Desktop/UTARMS-K
+
+
+
---
+++ umount.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo umount /home/sommerse/Desktop/UTARMS-K
