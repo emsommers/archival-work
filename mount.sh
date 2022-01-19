33233a4d139418db850855219da2fb2e93e39134
Emily Sommers <sommers.emily@gmail.com>
2022-01-19 12:28:44 -0500

    update username

--- mount.sh
+++ mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo mount.cifs -v -o username=sommerse,uid=sommerse //190-77.library.utoronto.ca/ArchivesDigital$ /home/sommerse/Desktop/UTARMS-K
+sudo mount.cifs -v -o username=sommers2,uid=sommerse //190-77.library.utoronto.ca/ArchivesDigital$ /home/sommerse/Desktop/UTARMS-K
 
 
 
